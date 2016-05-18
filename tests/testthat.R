c796ec5cdf1bee436944bac175d89a6ed320342d
Nigel Delaney <spamavoid@outlook.com>
2016-05-18 14:36:34 -0700

    Add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(pbbamr)
+
+test_check("pbbamr")
---
+++ tests/testthat/test.load.R
@@ -0,0 +1,25 @@
+library(pbbamr)
+#bfile = "/Users/nigel/git/pbbamr/tests/testthat//test.aligned.bam"
+#ffile = "/Users/nigel/git/pbbamr/tests/testthat/lambdaNEB.fa"
+#ofile = "/Users/nigel/git/pbbamr/tests/testthat/loadedAln.Rd"
+
+bfile = "test.aligned.bam"
+ffile = "lambdaNEB.fa"
+ofile = "loadedAln.Rd"
+
+test_that("dataMatches", {
+  d = loadpbi(bfile)
+  head(d)
+  aln = loadDataAtOffsets(d$offset, bfile, ffile)
+  #org_aln = aln
+  #save(org_aln, file=ofile)
+  load(ofile)
+  for (i in 1:length(org_aln)) {
+    expect_equal(any(aln[[i]] != org_aln[[i]]), FALSE)
+  }
+})
+
+test_that("headerMatches", {
+  d = loadheader(bfile)
+  expect_equal(as.numeric(as.character(d$sequences$length[1])), 48502)
+})
