5ef8f3a320ea82ba47f03793d5ccf1edeed05c2a
Nigel Delaney <spamavoid@outlook.com>
2016-05-19 15:28:15 -0700

    Remove outdated files

--- update_pbbam.sh
+++ update_pbbam.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+# Remove the old stuff
 #mkdir ~/git/pbbamr/src/pbbam/
+find ~/git/pbbamr/src/pbbam -name "*.cpp" -exec rm -rf {} \;
+find ~/git/pbbamr/src/pbbam -name "*.h" -exec rm -rf {} \;
+rm -rf ~/git/pbbamr/inst/include/pbbam
+# Copy the new stuff
 cp -r ~/git/pbbam/src/ ~/git/pbbamr/src/pbbam/
 cp -r ~/git/pbbam/include/pbbam ~/git/pbbamr/inst/include/
 # ditch the swig stuff.
