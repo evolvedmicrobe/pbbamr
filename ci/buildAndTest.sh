87c7a7a882ab67054cd45e85ea008ed671d245fe
Nigel Delaney <nigel.delaney@outlook.com>
2016-05-19 15:50:12 -0700

    Run Tests in CircleCI

--- ci/buildAndTest.sh
+++ ci/buildAndTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script will be run within the Docker container to build and test.
-# TODO: how do we run the tests?
 R CMD INSTALL .
+Rscript -e 'library(testthat); test_file("tests/testthat.R");'
